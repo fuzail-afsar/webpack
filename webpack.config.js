d49515dfc46e97a7959eef6178e802b40d46d14b
Fuzail <fuzail@mtpixels.com>
2022-04-19 17:04:08 +0500

    Authoring Libraries use our library without lodash library

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "webpack-numbers.js",
-    library: "webpackNumbers", //export libarary as global variable for access
+    // library: "webpackNumbers", //export libarary as global variable for access
+    library: {
+      name: "webpackNumbers",
+      type: "umd",
+    },
     clean: true,
+  },
+  externals: {
+    //donot include external library in your library build (external library is required for user to use your library)
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
+    },
   },
 };
