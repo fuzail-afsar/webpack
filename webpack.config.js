0f7cd3fabfa829df82b47d9bc9bd3b98103d7ee3
Fuzail <fuzail@mtpixels.com>
2022-04-19 14:42:53 +0500

    code splitting for dynamic imports

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,6 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -19,6 +14,5 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
 };
