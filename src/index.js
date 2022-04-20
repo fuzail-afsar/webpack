154c4b1158c1fe74ed8265a745a64e68cb5bba7e
Fuzail <fuzail@mtpixels.com>
2022-04-20 12:30:13 +0500

    Lazy Loading load file on button click

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import _ from "lodash";
-import Print from "./print";
 
 function component() {
   const element = document.createElement("div");
-
-  // Lodash, now imported by this script
+  const button = document.createElement("button");
+  const br = document.createElement("br");
+  button.innerHTML = "Click me and look at the console!";
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print.bind(null, "Hello webpack!");
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
+
+      print();
+    });
 
   return element;
 }
